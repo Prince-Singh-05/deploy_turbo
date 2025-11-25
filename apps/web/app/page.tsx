08f60960952a13537fd69d3c40b582b8b8939f69
Prince-Singh-05 <luciefur6393473659@gmail.com>
2025-11-25 06:11:48 +0530

    testing cd pipeline

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,9 +4,17 @@ export default async function Home() {
   const user = await client.user.findFirst();
 
   return (
-    <div>
-      {user?.username}
-      {user?.password}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "20px",
+        gap: "8px",
+      }}
+    >
+      <span>User Name: {user?.username}</span>
+      <span>Password: {user?.password}</span>
     </div>
   );
 }
